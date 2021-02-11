434dd01ac6798379d8e91dad7fb7372a098a9105
Tihomir_K <dronaldo12345@gmail.com>
2021-02-11 12:21:51 +0400

    fixed writing of positions into csv

--- ActionListener.py
+++ ActionListener.py
@@ -36,10 +36,12 @@ class ActionListener(Leap.Listener):
         # Get hands
         for hand in frame.hands:
 
-            vector = [-1 if hand.is_left else 1, hand.palm_position,
+            vector = [-1 if hand.is_left else 1, hand.palm_position[0], hand.palm_position[1], hand.palm_position[2],
                       hand.palm_normal.roll, hand.direction.pitch,
-                      hand.direction.yaw, hand.arm.direction,
-                      hand.arm.wrist_position, hand.arm.elbow_position]
+                      hand.direction.yaw,
+                      hand.arm.direction[0], hand.arm.direction[1], hand.arm.direction[2],
+                      hand.arm.wrist_position[0], hand.arm.wrist_position[1], hand.arm.wrist_position[2],
+                      hand.arm.elbow_position[0], hand.arm.elbow_position[1], hand.arm.elbow_position[2]]
 
             # Get fingers
             for finger in hand.fingers:
@@ -47,7 +49,9 @@ class ActionListener(Leap.Listener):
                 # Get bones
                 for b in range(0, 4):
                     bone = finger.bone(b)
-                    vector.extend([bone.prev_joint, bone.next_joint, bone.direction])
+                    vector.extend([bone.prev_joint[0], bone.prev_joint[1], bone.prev_joint[2],
+                                   bone.next_joint[0], bone.next_joint[1], bone.next_joint[2],
+                                   bone.direction[0], bone.direction[1], bone.direction[2]])
 
             self.writer.writerow(vector)
 
