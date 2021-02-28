8ae38f4c94a15205e9ea900f8416b159f7116792
Tihomir_K <dronaldo12345@gmail.com>
2021-02-28 16:44:47 +0300

    fixed bag with not existed folder collected_data and added header to csv

--- ActionListener.py
+++ ActionListener.py
@@ -1,6 +1,7 @@
 import Leap
 from datetime import datetime
 import csv
+import os
 
 
 class ActionListener(Leap.Listener):
@@ -12,12 +13,28 @@ class ActionListener(Leap.Listener):
     creates in folder ./collected_data csv file with all frames collected during to session
     """
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
-    bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
 
     def __init__(self, letter):
         Leap.Listener.__init__(self)
+        if not os.path.exists("collected_data"):
+            os.mkdir("collected_data")
         self.file = open("./collected_data/" + letter + "_" + datetime.now().strftime("%d-%m_%H-%M-%S") + ".csv", "w")
         self.writer = csv.writer(self.file)
+        header = ['hand_type',
+                  'position_x', 'position_y', 'position_z',
+                  'roll', 'pitch', 'yaw',
+                  'arm_dir_x', 'arm_dir_y', 'arm_dir_z',
+                  'wrist_pos_x', 'wrist_pos_y', 'wrist_pos_z',
+                  'elbow_pos_x', 'elbow_pos_y', 'elbow_pos_z']
+
+        for i in range(5):
+            header.extend([self.finger_names[i] + '_finger_id', 'finger_length', 'finger_width'])
+            for b in range(0, 4):
+                header.extend(['prev_joint_x', 'prev_joint_y', 'prev_joint_z',
+                               'next_joint_x', 'next_joint_y', 'next_joint_z',
+                               'direction_x', 'direction_y', 'direction_z'])
+
+        self.writer.writerow(header)
 
     def on_init(self, controller):
         print "Initialized"
@@ -43,9 +60,9 @@ class ActionListener(Leap.Listener):
         # Get hands
         for hand in frame.hands:
 
-            vector = [-1 if hand.is_left else 1, hand.palm_position[0], hand.palm_position[1], hand.palm_position[2],
-                      hand.palm_normal.roll, hand.direction.pitch,
-                      hand.direction.yaw,
+            vector = [-1 if hand.is_left else 1,
+                      hand.palm_position[0], hand.palm_position[1], hand.palm_position[2],
+                      hand.palm_normal.roll, hand.direction.pitch, hand.direction.yaw,
                       hand.arm.direction[0], hand.arm.direction[1], hand.arm.direction[2],
                       hand.arm.wrist_position[0], hand.arm.wrist_position[1], hand.arm.wrist_position[2],
                       hand.arm.elbow_position[0], hand.arm.elbow_position[1], hand.arm.elbow_position[2]]
